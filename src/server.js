494ad2fce4c7a741950601ffef2691efa9fe69c1
duythong114 <tranthong.facebook@gmail.com>
2024-02-21 14:11:34 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require('dotenv').config();
+
+let app = express();
+
+// config app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+// Port === undefined => port = 6969
+let port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
